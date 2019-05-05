2e79a9e11fb9888cf67f2ae4e16bac1134115d37
Nicholas Acosta <nickacosta93@gmail.com>
2019-05-05 01:37:11 -0400

    Updated includes

--- common.h
+++ common.h
@@ -3,7 +3,19 @@
 
 /*======STANDARD TEMPLATE=======*/
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <memory>
+#include <ctime>
+#include <vector>
 
 using namespace std;
+
+/*=======GLU/GLEW=========*/
+#include "glew.h"
+
+/*=======SDL2=======*/
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
 
 #endif
